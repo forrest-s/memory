4d5784f9fc796ecb9a0bf167b10979bbc2088974
Forrest S <thewanderinwolves@gmail.com>
2023-09-02 16:23:47 -0700

    added functionality for multiple players.
    added reset button.
    up to four players allowed.
    scoreboard added.
    styling updated.

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ const deck = [
     '6 ♣ ', '7 ♣ ', '8 ♣ ', '9 ♣ ', '10 ♣ ',
     'Jack ♣ ', 'Queen ♣ ', 'King ♣ ', 'Joker', 'Joker'
 ];
-const symbols = [...deck]; 
 let cards = [];
 let flippedCards = [];
 let moveCount = 0;
 let checking = false; // Flag to indicate if the cards are being checked
+const players = ['Player One']; // Add as many players as needed
 
 const gameContainer = document.getElementById('game-container');
 const startButton = document.getElementById('start-button');
@@ -24,14 +24,35 @@ const moveCounter = document.getElementById('move-counter');
 const winDialog = document.getElementById('win-dialog');
 const matchesCount = document.getElementById('matches-count');
 const turnsCount = document.getElementById('turns-count');
+const winnerName = document.getElementById('winner-name')
+const totalPlayers = document.getElementById('num-players');
+const currentPlayerLabel = document.getElementById('current-player-label')
+const scoreboard = document.getElementById('scoreboard')
+const resetButton = document.getElementById('reset-game')
+
+resetButton.addEventListener('click', () => resetGame())
 
 // Start the game when the "Start Game" button is clicked
 startButton.addEventListener('click', () => {
     createCards();
-    startButton.disabled = true;
+    currentPlayerLabel.textContent = players[0]
+    if (totalPlayers.value === '2') players.push('Player Two')
+    if (totalPlayers.value === '3') players.push('Player Two', 'Player Three')
+    if (totalPlayers.value === '4') players.push('Player Two', 'Player Three', 'Player Four')
+    for (const player of players) {
+        const playerMatches = document.createElement('div')
+        playerMatches.textContent = `${player} total matches: `
+        const playerMatchesTotal = document.createElement('span')
+        playerMatchesTotal.setAttribute('id', player)
+        playerMatchesTotal.textContent = 0
+        playerMatches.append(playerMatchesTotal)
+        scoreboard.append(playerMatches)
+    }
+    startButton.style.display = 'none';
+    resetButton.style.display = 'block'
 });
 
-// Shuffle the symbols array
+// Shuffle the deck array
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -41,37 +62,18 @@ function shuffleArray(array) {
 
 // Create card elements and add them to the game container
 function createCards() {
-    shuffleArray(symbols);
+    shuffleArray(deck);
     cards = []; // Clear previous cards
-    gameContainer.innerHTML = '';
     moveCount = 0;
     moveCounter.textContent = moveCount;
 
-    for (const symbol of symbols) {
+    for (const card of deck) {
         const cardElement = document.createElement('div');
         cardElement.className = 'card';
-        cardElement.dataset.symbol = symbol; // Store the symbol as a data attribute
-
-        cardElement.addEventListener('click', () => flipCard(cardElement));
+        cardElement.dataset.card = card; // Store the card as a data attribute
+        cardElement.addEventListener('click', () => handleCardClick(cardElement));
         gameContainer.appendChild(cardElement);
         cards.push(cardElement);
-    }
-}
-
-// Event listener for card clicks
-function flipCard(card) {
-    if (!flippedCards.includes(card) && flippedCards.length < 2 && !checking) {
-        card.classList.add('flipped');
-        card.textContent = card.dataset.symbol; // Display the card's symbol
-
-        flippedCards.push(card);
-
-        if (flippedCards.length === 2) {
-            moveCount++;
-            moveCounter.textContent = moveCount;
-            checking = true; // Set checking to true while cards are being checked
-            checkMatch();
-        }
     }
 }
 
@@ -81,37 +83,29 @@ function checkMatch() {
     
     const value1 = card1.textContent.split(' ')[0]; // Get the value of the first card
     const value2 = card2.textContent.split(' ')[0]; // Get the value of the second card
-    
+    let matched = false
     
     if (value1 === value2) {
+        const playerNumber = currentPlayerLabel.textContent.split('')
+        playerNumber.splice(6, 1) // remove space
         card1.classList.add('matched');
+        card1.setAttribute('id', playerNumber.join(''))
         card2.classList.add('matched');
+        card2.setAttribute('id', playerNumber.join(''))
+        matched = true
     } else {
         setTimeout(() => {
             card1.classList.remove('flipped');
             card2.classList.remove('flipped');
             card1.textContent = ''; // Hide the value
             card2.textContent = ''; // Hide the value
-        }, 500); // Delay before flipping back
+        }, 230); // Delay before flipping back
     }
 
     flippedCards = [];
     checking = false; // reset flag
 
-    if (document.querySelectorAll('.matched').length === symbols.length) openDialog(cards.length / 2, moveCount)
-}
-
-// Function to open the modal when the game is won
-function openDialog(matches, turns) {
-    matchesCount.textContent = matches;
-    turnsCount.textContent = turns;
-    winDialog.showModal();
-}
-
-// Function to close the modal
-function closeDialog() {
-    winDialog.close();
-    resetGame();
+    return matched
 }
 
 // Function to reset the game board
@@ -123,11 +117,95 @@ function resetGame() {
     while (gameContainer.firstChild) {
         gameContainer.removeChild(gameContainer.firstChild)
     }
+    while (scoreboard.firstChild) {
+        scoreboard.removeChild(scoreboard.firstChild)
+    }
+    currentPlayerLabel.textContent = ''
+    totalPlayers.value = 1
+    players.splice(1)
     // Make the "Start Game" button active again
-    const startButton = document.getElementById('start-button');
-    startButton.disabled = false;
+    startButton.style.display = 'block';
+    resetButton.style.display = 'none'
 }
 
 // Attach an event listener to the close button
 const closeButton = document.getElementById('close-dialog-button');
 closeButton.addEventListener('click', closeDialog);
+
+function switchToNextPlayer() {
+    if (players.length === 1) return
+    else if (players.length === 2) {
+        if (currentPlayerLabel.textContent === 'Player One') return currentPlayerLabel.textContent = players[1]
+        if (currentPlayerLabel.textContent === 'Player Two') return currentPlayerLabel.textContent = players[0]
+    } else if (players.length === 3) {
+        if (currentPlayerLabel.textContent === 'Player One') return currentPlayerLabel.textContent = players[1]
+        if (currentPlayerLabel.textContent === 'Player Two') return currentPlayerLabel.textContent = players[2]
+        if (currentPlayerLabel.textContent === 'Player Three') return currentPlayerLabel.textContent = players[0]
+    } else if (players.length === 4) {
+        if (currentPlayerLabel.textContent === 'Player One') return currentPlayerLabel.textContent = players[1]
+        if (currentPlayerLabel.textContent === 'Player Two') return currentPlayerLabel.textContent = players[2]
+        if (currentPlayerLabel.textContent === 'Player Three') return currentPlayerLabel.textContent = players[3]
+        if (currentPlayerLabel.textContent === 'Player Four') return currentPlayerLabel.textContent = players[0]
+    }
+}
+
+function handleCardClick(card) {
+    if (!flippedCards.includes(card) && flippedCards.length < 2 && !checking) {
+        card.classList.add('flipped');
+        card.textContent = card.dataset.card; // Display the card's card
+
+        flippedCards.push(card);
+
+        if (flippedCards.length === 2) {
+            moveCount++;
+            moveCounter.textContent = moveCount;
+            checking = true; // Set checking to true while cards are being checked
+            let isMatch = checkMatch();
+            if (isMatch) {
+                const matchTotal = document.getElementById(currentPlayerLabel.textContent)
+                matchTotal.textContent++
+                if (document.querySelectorAll('.matched').length === deck.length) return displayWinner()
+            } else {
+                // Switch to the next player's turn
+                switchToNextPlayer();
+            }
+        }
+    }
+}
+
+function displayWinner() {
+    if (players.length === 1) {
+        const totalMatches = document.getElementById(currentPlayerLabel.textContent)
+        openDialog(totalMatches.textContent, moveCount, players[0])
+        return 
+    } 
+    // get the high score
+    const allScores = scoreboard.querySelectorAll('span')
+    let maxScore = 0
+    for (score of allScores) {
+        if (~~score.textContent > maxScore) maxScore = ~~score.textContent
+    }
+    // check for duplicates
+    let winners = []
+    for (const [index, value] of allScores.entries()) {
+        if (~~value.textContent === maxScore) winners.push(value.id)
+    }
+
+    if (winners.length > 1) return openDialog(maxScore, moveCount, winners.join(', '))
+
+    return openDialog(maxScore, moveCount, winners[0])
+}
+
+// Function to open the modal when the game is won
+function openDialog(matches, turns, winner) {
+    matchesCount.textContent = matches;
+    turnsCount.textContent = turns;
+    winnerName.textContent = winner;
+    winDialog.showModal();
+}
+
+// Function to close the modal
+function closeDialog() {
+    winDialog.close();
+    resetGame();
+}
